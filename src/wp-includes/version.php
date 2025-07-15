8f62b5b5a17a851afb087b4ac650f9f49e8889f1
Jb Audras <audrasjb@git.wordpress.org>
2025-07-15 15:29:11 +0000

    Post 6.8.2 release bump.
    The 6.8 branch is now 6.8.3-alpha
    
    
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.8@60474 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.8.2-src';
+$wp_version = '6.8.3-alpha-60474-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
