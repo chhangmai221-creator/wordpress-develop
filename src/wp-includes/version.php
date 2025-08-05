754efd389e599663f1e13546809ab592b7bf51eb
David Baumwald <davidbaumwald@git.wordpress.org>
2025-08-05 20:07:18 +0000

    Post 6.7.3 release bump.
    The 6.7 branch is now 6.7.4-alpha.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.7@60583 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.7.3-src';
+$wp_version = '6.7.4-alpha-60583-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
