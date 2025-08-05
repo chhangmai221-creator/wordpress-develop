29c10f98f68da5cbd17737d7a5532840f013abc2
David Baumwald <davidbaumwald@git.wordpress.org>
2025-08-05 20:30:39 +0000

    Post 6.0.10 release bump.
    The 6.0 branch is now 6.0.11-alpha.
    
    git-svn-id: https://develop.svn.wordpress.org/branches/6.0@60595 602fd350-edb4-49c9-b593-d223f7449a82

--- src/wp-includes/version.php
+++ src/wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '6.0.10-src';
+$wp_version = '6.0.11-alpha-60595-src';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
